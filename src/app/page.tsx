a8ca0d093bc90e835b706e709fbad7cc79ac7678
Fouad-berry <fidriss96@gmail.com>
2025-05-07 09:07:54 +0200

    [FIX] fix responsive on mobile screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ const page = () => {
 
       {/* Section 4 */}
 
-      <div className="relative w-full h-[600px]">
+      <div className="relative w-full min-h-[600px]">
         <Image
           src={banner}
           alt="Bannière"
@@ -52,24 +52,23 @@ const page = () => {
           className="object-cover"
           priority
         />
-        <div className="absolute inset-0 flex flex-col justify-center pl-10 text-white text-left px-4 w-full sm:w-3/4 lg:w-1/2 z-10">
-          <h1 className="text-2xl font-bold mb-2 text-left">Notre histoire</h1>
-          <p className="text-sm font-extralight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quod voluptas. Repellendus nam reprehenderit atque, culpa eum placeat quas dolores quibusdam qui delectus, ex odio sequi! Quas, cupiditate atque. Ab.</p>
-          <p className="text-sm font-extralight mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus est ullam cupiditate? Recusandae nesciunt nostrum odit, velit numquam eos nihil nemo quibusdam possimus, totam maiores tempore! Aspernatur suscipit omnis accusantium.</p>
-          <p className="text-sm font-extralight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quod voluptas. Repellendus nam reprehenderit atque, culpa eum placeat quas dolores quibusdam qui delectus, ex odio sequi! Quas, cupiditate atque. Ab.</p>
-          <p className="text-sm font-extralight mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus est ullam cupiditate? Recusandae nesciunt nostrum odit, velit numquam eos nihil nemo quibusdam possimus, totam maiores tempore! Aspernatur suscipit omnis accusantium.</p>
-          <p className="text-sm font-extralight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quod voluptas. Repellendus nam reprehenderit atque, culpa eum placeat quas dolores quibusdam qui delectus, ex odio sequi! Quas, cupiditate atque. Ab.</p>
-          <p className="text-sm font-extralight mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus est ullam cupiditate? Recusandae nesciunt nostrum odit, velit numquam eos nihil nemo quibusdam possimus, totam maiores tempore! Aspernatur suscipit omnis accusantium.</p>
-          <div className="self-start">
-            <Button title="Découvrir notre histoire" route="/produits" />
+        <div className="absolute inset-0 flex items-center justify-start px-4 py-10 z-10">
+          <div className="text-white text-left max-w-2xl w-full">
+            <h1 className="text-2xl font-bold mb-4">Notre histoire</h1>
+            <div className="space-y-4 text-sm font-extralight">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+            </div>
+            <div className="mt-6">
+              <Button title="Découvrir notre histoire" route="/produits" />
+            </div>
           </div>
         </div>
       </div>
       
-
       {/* Section 5 */}
-      
-      <div className="relative w-full h-[600px]">
+      <div className="relative w-full min-h-[600px]">
         <Image
           src={banner}
           alt="Bannière"
@@ -77,38 +76,20 @@ const page = () => {
           className="object-cover"
           priority
         />
-
-        <div className="absolute inset-0 flex flex-col justify-center items-end px-4 z-10">
-              <div className="w-full sm:w-3/4 lg:w-1/2 pr-10 text-white text-right">
-            <h1 className="text-2xl font-bold mb-2">Nos produits</h1>
-
-            <p className="text-sm font-extralight">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nulla beatae ducimus aperiam! Quibusdam illum inventore quas earum soluta quo. Repellat reiciendis consequatur quo nam repudiandae velit dolore voluptatum ab.
-            </p>
-            <p className="text-sm font-extralight mb-5">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic sequi, laudantium sapiente nobis impedit quo omnis soluta dolore necessitatibus numquam vero possimus reiciendis, ad deserunt eius totam molestias, officia obcaecati!
-            </p>
-            <p className="text-sm font-extralight">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel ea, veritatis tempora rem soluta alias voluptatum laboriosam veniam numquam, accusantium ut dignissimos et aperiam maxime eius cupiditate, sed nam quam?
-            </p>
-            <p className="text-sm font-extralight mb-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, iste? Eos amet perspiciatis fugit harum id dolorum labore? Quasi, repellat. Officia dolore, corporis harum recusandae dolorum voluptates quam tenetur adipisci!
-            </p>
-            <p className="text-sm font-extralight">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, perferendis nobis dolorum id assumenda impedit quos reiciendis fuga dolores exercitationem cupiditate fugiat aliquid quod nostrum cum in, minima a qui.
-            </p>
-            <p className="text-sm font-extralight mb-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, itaque neque. In tempora amet facilis, eveniet iste numquam, ut quia dolorum aspernatur possimus nesciunt sint alias sapiente, perferendis dolor doloremque.
-            </p>
-
-            <div className="w-full flex justify-end mt-6">
-              <div className="w-[250px] flex flex-col gap-4">
-                <Button title="Découvrir nos whisky" route="/nos-whisky" />
-                <Button title="Découvrir nos cidres" route="/nos-cidres" />
-                <Button title="Découvrir nos vins" route="/nos-vins" />
-                <Button title="Découvrir nos exclusivités" route="/nos-exclusivités" />
-              </div>
-            </div>          
+        <div className="absolute inset-0 flex items-center justify-end px-4 py-10 z-10">
+          <div className="text-white text-right max-w-2xl w-full">
+            <h1 className="text-2xl font-bold mb-4">Nos produits</h1>
+            <div className="space-y-4 text-sm font-extralight">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+            </div>
+            <div className="mt-6 flex flex-col items-end gap-4">
+              <Button title="Découvrir nos whisky" route="/nos-whisky" />
+              <Button title="Découvrir nos cidres" route="/nos-cidres" />
+              <Button title="Découvrir nos vins" route="/nos-vins" />
+              <Button title="Découvrir nos exclusivités" route="/nos-exclusivités" />
+            </div>
           </div>
         </div>
       </div>
